51589cd3fd6bdd74d2702ce963e9454f05c1d71e
Jagadish-git1 <87804732+Jagadish-git1@users.noreply.github.com>
2022-11-18 11:24:59 +0530

    delete

--- Middleware/NotFound.js
+++
@@ -1,5 +0,0 @@
-const notFound = (req, res) => {
-  res.status(404).send("Route/Link not found!!!");
-};
-
-module.exports = notFound;
--- Middleware/errorHandler.js
+++
@@ -1,9 +0,0 @@
-const { CustomApiError } = require("../Errors/CustomError");
-const errorHandler = (err, req, res, next) => {
-  if (err instanceof CustomApiError) {
-    return res.status(err.statusCode).json({ msg: err.message });
-  }
-  return res.status(500).json({ msg: "Something Went wrong" });
-};
-
-module.exports = errorHandler;
